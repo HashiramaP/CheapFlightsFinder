1df022618ea495ecb8b4ad54d7b78e9aac885dd4
Homayouni <parsa.homayouni.1@ulaval.ca>
2023-10-15 19:13:48 -0400

    first commit

---
+++ AutoRun.py
@@ -0,0 +1,56 @@
+import Flight
+from Flight import InitialLink, FindFlights
+from time import sleep
+
+t_f = True
+iter = 0
+previous_result = None  # Initialize the variable to store the previous result
+
+print("Welcome to Cheap Flight Finder!")
+
+#ask which travel destinations
+
+vacances = []
+
+dest = input("Enter a travel destination (Ex: Amérique du nord): ")
+vacances.append(dest)
+con = input("Do you wish to add another destination? (Yes/No): ")
+
+while con.upper() == "YES":
+    dest = input("Enter a travel destination (Ex: Amérique du nord): ")
+    vacances.append(dest)
+    con = input("Do you wish to add another destination? (Yes/No): ")
+
+
+#ask which departure city
+city = input("Enter a departure city (Ex: Montréal): ")
+
+#ask which dates
+date1 = input("Enter a departure date (Ex: 23 octobre): ")
+date2 = input("Enter a return date (Ex: 29 octobre): ")
+
+#enter the maximum price
+
+price = input("Enter the maximum price you are willing to pay without the dollar sign (Ex: 500): ")
+
+lien = InitialLink(vacances, city, [date1, date2])
+
+while t_f:
+    iter += 1
+    print(iter)
+    if iter == 2000:
+        t_f = False
+    else:
+        current_result = FindFlights(lien, price)
+        if len(current_result) == 0:
+            print("No results found!")
+        
+        # Print only if the current result is different from the previous result
+        if current_result != previous_result:
+            print("New result found!")
+            print(current_result)
+        
+        # Update the previous result for the next iteration
+        previous_result = current_result
+
+        sleep(60)
---
+++ Flight.py
@@ -0,0 +1,167 @@
+import openpyxl
+import bs4
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
+from openpyxl import Workbook, load_workbook
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+
+vacances = ["Caraibes", "Amérique du Nord"]
+
+
+def InitialLink(dests, ville, dates):
+
+    url_list = []
+
+    driver = webdriver.Chrome()
+
+    driver.get("https://www.google.com/travel/flights?tcfs&ved=2ahUKEwi_5dndsrCBAxUmi8UCHZEWAosQ0I8EegQIAxAK&ictx=2")
+
+    for i, dest in enumerate(dests):
+            if i == 0:
+
+                case = driver.find_elements(By.CLASS_NAME, "II2One")
+
+                case[0].clear() # clear the cell
+                case[0].send_keys(ville) # add departure city
+                sleep(2)
+                search_dep = driver.find_elements(By.CLASS_NAME, "CwL3Ec") # find the city button
+                search_dep[0].click() # click the city button
+
+
+                case[2].send_keys(dest) # add arrival city
+                sleep(1)
+                search_ariv2 = driver.find_elements(By.CLASS_NAME, "CwL3Ec") # find the city button
+                search_ariv2[0].click() # click the city button
+
+                date2 = driver.find_elements(By.CLASS_NAME, "TP4Lpb ") # find the date button
+
+                sleep(2)
+
+                date2[0].click() # click the date button
+                date2[0].send_keys(dates[0]) # click the date button
+                date2[0].send_keys(Keys.ENTER) # click the date button
+                date2[0].send_keys(Keys.ENTER) # click the date button
+
+                sleep(2)
+
+                date2[3].click() # click the date button
+                date2[3].send_keys(dates[1]) # click the date button
+                date2[3].send_keys(Keys.ENTER) # click the date button
+                date2[3].send_keys(Keys.ENTER) # click the date button
+                
+                sleep(3)
+
+                current_url = driver.current_url
+
+                zizi = driver.find_elements(By.CLASS_NAME, "II2One")
+                zizi[2].click() # click the date button
+                zizi[2].send_keys(Keys.ENTER)
+                zizi[2].send_keys(Keys.ENTER)
+
+                sleep(10)
+
+                url_list += [[dest, driver.current_url]]
+
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+                driver.quit()
+
+            if i >= 1:
+                drive = webdriver.Chrome()
+                drive.get(current_url)
+
+                case = drive.find_elements(By.CLASS_NAME, "II2One")
+                case[2].clear() # clear the cell
+                case[2].send_keys(dest)
+                sleep(1) 
+                search_dep = drive.find_elements(By.CLASS_NAME, "CwL3Ec") # find the city button
+                search_dep[0].click() # click the city button
+                case[2].send_keys(Keys.ENTER)
+                case[2].send_keys(Keys.ENTER)
+
+                sleep(10)
+
+                url_list += [[dest, drive.current_url]]
+
+                drive.quit()
+
+    return url_list
+
+def FindFlights(url_list, aim):
+
+
+    voyage = []
+    for j in url_list:
+            dest = j[0]
+            link = j[1]
+            driver = webdriver.Chrome()
+            driver.get(link)
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            
+            try:
+                main = WebDriverWait(driver, 30).until( EC.presence_of_all_elements_located((By.CLASS_NAME, "SD4Ugf")))   
+                destinations = driver.find_elements(By.CLASS_NAME, "lPyEac")
+                for z, destination in enumerate(destinations):
+                    if z >= 0:
+                        City = destination.find_element(By.CLASS_NAME, "W6bZuc")
+                        Price = destination.find_element(By.CLASS_NAME, "QB2Jof")
+                        
+                        
+                        price_with_non_numeric = Price.text
+                        cleaned_price = ''.join(filter(str.isdigit, price_with_non_numeric))  # Remove non-numeric characters
+                        prix = float(cleaned_price)
+
+                        if dest == "Amérique du Nord":
+                            if City.text == "Cancún" or City.text == "Miami" or City.text == "Honolulu":
+                                if prix <= aim:
+                                    voyage += [[City.text, prix]]
+
+                                    
+                        else:               
+                            if prix <= aim:
+                                voyage += [[City.text, prix]]
+            except:
+                pass
+            
+            finally:
+                driver.quit()
+
+    return voyage
+
+def GetLinks(url_list, fly1):
+    links = []
+    for j in url_list:
+            dest = j[0]
+            link = j[1]
+            driver = webdriver.Chrome()
+            driver.get(link)
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            try:
+                main = WebDriverWait(driver, 30).until( EC.presence_of_all_elements_located((By.CLASS_NAME, "SD4Ugf")))   
+                destinations = driver.find_elements(By.CLASS_NAME, "lPyEac")
+                for z, destination in enumerate(destinations):
+                    if z >= 0:
+                        City = destination.find_element(By.CLASS_NAME, "W6bZuc")
+                        for enum in fly1:
+                            if City.text == enum[0]:
+                                destination.click()
+                                sleep(2)
+                                #add the href to the list
+                                li = driver.find_element(By.CLASS_NAME, "DvoDQ")
+                                links += [[li.get_attribute("href")]]
+                                driver.back()
+            except:
+                pass
+    return links
+
+#lien = [['Caraibes', 'https://www.google.com/travel/explore?tfs=CBwQAxooEgoyMDIzLTEyLTI3agwIAxIIL20vMDUycDdyDAgEEggvbS8wMjYxbRooEgoyMDI0LTAxLTA1agwIBBIIL20vMDI2MW1yDAgDEggvbS8wNTJwN0ABSAFwAYIBCwj___________8BmAEBsgEEGAEgAQ&tfu=GgAqAggD'], ['Amérique du Nord', 'https://www.google.com/travel/explore?tfs=CBwQAxooEgoyMDIzLTEyLTI3agwIAxIIL20vMDUycDdyDAgEEggvbS8wNTlnNBooEgoyMDI0LTAxLTA1agwIBBIIL20vMDU5ZzRyDAgDEggvbS8wNTJwN0ABSAFwAYIBCwj___________8BmAEBsgEEGAEgAQ&tfu=GgAqAA']]
+
+#fly = [['Grand Cayman', 805.0], ['Miami', 839.0], ['Cancún', 703.0]]
+
+#print(GetLinks(lien, fly))
