b6350ad1d21e193c1d7787033349dfba85fd2dff
Homayouni <parsa.homayouni.1@ulaval.ca>
2023-10-17 16:08:30 -0400

    Link functions

--- Flight.py
+++ Flight.py
@@ -12,88 +12,72 @@ from time import sleep
 
 vacances = ["Caraibes", "Amérique du Nord"]
 
+def FirstLink(ville, dates):
+     
+    first = []
 
-def InitialLink(dests, ville, dates):
-
-    url_list = []
-
+     
     driver = webdriver.Chrome()
 
     driver.get("https://www.google.com/travel/flights?tcfs&ved=2ahUKEwi_5dndsrCBAxUmi8UCHZEWAosQ0I8EegQIAxAK&ictx=2")
 
-    for i, dest in enumerate(dests):
-            if i == 0:
+    case = driver.find_elements(By.CLASS_NAME, "II2One")
 
-                case = driver.find_elements(By.CLASS_NAME, "II2One")
+    case[0].clear() # clear the cell
+    case[0].send_keys(ville) # add departure city
+    sleep(2)
+    search_dep = driver.find_elements(By.CLASS_NAME, "CwL3Ec") # find the city button
+    search_dep[0].click() # click the city button
 
-                case[0].clear() # clear the cell
-                case[0].send_keys(ville) # add departure city
-                sleep(2)
-                search_dep = driver.find_elements(By.CLASS_NAME, "CwL3Ec") # find the city button
-                search_dep[0].click() # click the city button
+    date2 = driver.find_elements(By.CLASS_NAME, "TP4Lpb ") # find the date button
 
+    sleep(2)
 
-                case[2].send_keys(dest) # add arrival city
-                sleep(1)
-                search_ariv2 = driver.find_elements(By.CLASS_NAME, "CwL3Ec") # find the city button
-                search_ariv2[0].click() # click the city button
+    date2[0].click() # click the date button
+    date2[0].send_keys(dates[0]) # click the date button
+    date2[0].send_keys(Keys.ENTER) # click the date button
+    date2[0].send_keys(Keys.ENTER) # click the date button
 
-                date2 = driver.find_elements(By.CLASS_NAME, "TP4Lpb ") # find the date button
+    sleep(2)
 
-                sleep(2)
-
-                date2[0].click() # click the date button
-                date2[0].send_keys(dates[0]) # click the date button
-                date2[0].send_keys(Keys.ENTER) # click the date button
-                date2[0].send_keys(Keys.ENTER) # click the date button
-
-                sleep(2)
-
-                date2[3].click() # click the date button
-                date2[3].send_keys(dates[1]) # click the date button
-                date2[3].send_keys(Keys.ENTER) # click the date button
-                date2[3].send_keys(Keys.ENTER) # click the date button
+    date2[3].click() # click the date button
+    date2[3].send_keys(dates[1]) # click the date button
+    date2[3].send_keys(Keys.ENTER) # click the date button
+    date2[3].send_keys(Keys.ENTER) # click the date button
                 
-                sleep(3)
+    sleep(3)
 
-                current_url = driver.current_url
+    first = driver.current_url
 
-                zizi = driver.find_elements(By.CLASS_NAME, "II2One")
-                zizi[2].click() # click the date button
-                zizi[2].send_keys(Keys.ENTER)
-                zizi[2].send_keys(Keys.ENTER)
+    return first
 
-                sleep(10)
+def InitialLinks(link, dests):
 
-                url_list += [[dest, driver.current_url]]
+    url_list = []
 
-                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    for dest in dests:
+        driver = webdriver.Chrome()
 
-                driver.quit()
+        driver.get(link)
 
-            if i >= 1:
-                drive = webdriver.Chrome()
-                drive.get(current_url)
+        case = driver.find_elements(By.CLASS_NAME, "II2One")
+        case[2].clear() # clear the cell
+        case[2].send_keys(dest)
+        sleep(1) 
+        search_dep = driver.find_elements(By.CLASS_NAME, "CwL3Ec") # find the city button
+        search_dep[0].click() # click the city button
+        case[2].send_keys(Keys.ENTER)
+        case[2].send_keys(Keys.ENTER)
 
-                case = drive.find_elements(By.CLASS_NAME, "II2One")
-                case[2].clear() # clear the cell
-                case[2].send_keys(dest)
-                sleep(1) 
-                search_dep = drive.find_elements(By.CLASS_NAME, "CwL3Ec") # find the city button
-                search_dep[0].click() # click the city button
-                case[2].send_keys(Keys.ENTER)
-                case[2].send_keys(Keys.ENTER)
+        sleep(3)
 
-                sleep(10)
+        url_list += [[dest, driver.current_url]]
 
-                url_list += [[dest, drive.current_url]]
-
-                drive.quit()
+        driver.quit()
 
     return url_list
 
 def FindFlights(url_list, aim):
-
 
     voyage = []
     for j in url_list:
@@ -121,7 +105,6 @@ def FindFlights(url_list, aim):
                                 if prix <= aim:
                                     voyage += [[City.text, prix]]
 
-                                    
                         else:               
                             if prix <= aim:
                                 voyage += [[City.text, prix]]
@@ -133,33 +116,14 @@ def FindFlights(url_list, aim):
 
     return voyage
 
-def GetLinks(url_list, fly1):
-    links = []
-    for j in url_list:
-            dest = j[0]
-            link = j[1]
-            driver = webdriver.Chrome()
-            driver.get(link)
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+def GetLinks(link, dests):
+    new_desr = []
 
-            try:
-                main = WebDriverWait(driver, 30).until( EC.presence_of_all_elements_located((By.CLASS_NAME, "SD4Ugf")))   
-                destinations = driver.find_elements(By.CLASS_NAME, "lPyEac")
-                for z, destination in enumerate(destinations):
-                    if z >= 0:
-                        City = destination.find_element(By.CLASS_NAME, "W6bZuc")
-                        for enum in fly1:
-                            if City.text == enum[0]:
-                                destination.click()
-                                sleep(2)
-                                #add the href to the list
-                                li = driver.find_element(By.CLASS_NAME, "DvoDQ")
-                                links += [[li.get_attribute("href")]]
-                                driver.back()
-            except:
-                pass
-    return links
+    return True
+    
 
-lien = [['Caraibes', 'https://www.google.com/travel/explore?tfs=CBwQAxooEgoyMDIzLTEyLTI3agwIAxIIL20vMDUycDdyDAgEEggvbS8wMjYxbRooEgoyMDI0LTAxLTA1agwIBBIIL20vMDI2MW1yDAgDEggvbS8wNTJwN0ABSAFwAYIBCwj___________8BmAEBsgEEGAEgAQ&tfu=GgAqAggD'], ['Amérique du Nord', 'https://www.google.com/travel/explore?tfs=CBwQAxooEgoyMDIzLTEyLTI3agwIAxIIL20vMDUycDdyDAgEEggvbS8wNTlnNBooEgoyMDI0LTAxLTA1agwIBBIIL20vMDU5ZzRyDAgDEggvbS8wNTJwN0ABSAFwAYIBCwj___________8BmAEBsgEEGAEgAQ&tfu=GgAqAA']]
+lien = [['Amérique du Nord', 'https://www.google.com/travel/explore?tfs=CBwQAxooEgoyMDIzLTEyLTI3agwIAxIIL20vMDUycDdyDAgEEggvbS8wNTlnNBooEgoyMDI0LTAxLTA1agwIBBIIL20vMDU5ZzRyDAgDEggvbS8wNTJwN0ABSAFwAYIBCwj___________8BmAEBsgEEGAEgAQ&tfu=GgAqAA']]
+
+josh = "https://www.google.com/travel/flights?tfs=CBwQARoaEgoyMDIzLTEwLTIzagwIAxIIL20vMDUycDcaGhIKMjAyMy0xMC0yOXIMCAMSCC9tLzA1MnA3QAFIAXABggELCP___________wGYAQE&tfu=KgIIAw"
 
 print(FindFlights(lien, 850))
